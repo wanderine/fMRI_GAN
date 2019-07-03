dfdc192d3099d51e31a9c6f2e5eaceddd825c639
Anders Eklund <andek@c-10-254-187-89.clients.eduroam.liu.se>
2019-07-03 16:45:48 +0200

    Added download script for fcon1000

---
+++ download_fcon1000.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir Beijing Cambridge
+
+cd Beijing
+
+wget -O Beijing1.tar http://www.nitrc.org/frs/downloadlink.php/1602
+
+wget -O Beijing2.tar http://www.nitrc.org/frs/downloadlink.php/1604
+
+wget -O Beijing3.tar http://www.nitrc.org/frs/downloadlink.php/1605
+
+wget -O Beijing4.tar http://www.nitrc.org/frs/downloadlink.php/1606
+
+wget -O Beijing5.tar http://www.nitrc.org/frs/downloadlink.php/1607
+
+cd ..
+cd Cambridge
+
+wget -O Cambridge1.tar http://www.nitrc.org/frs/downloadlink.php/1608
+
+wget -O Cambridge2.tar  http://www.nitrc.org/frs/downloadlink.php/1609
+
+wget -O Cambridge3.tar  http://www.nitrc.org/frs/downloadlink.php/1610
+
+wget -O Cambridge4.tar  http://www.nitrc.org/frs/downloadlink.php/1611
+ 
